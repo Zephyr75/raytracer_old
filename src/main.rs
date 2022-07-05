fe8be800b2f33ed549b74eb6e41ba862e0d05d57
Zéphyr <antoine.familleroger@gmail.com>
2022-07-05 19:50:08 +0200

    Added ray contact with sphere

--- src/main.rs
+++ src/main.rs
@@ -38,22 +38,22 @@ fn get_pixels() -> Vec<Color>{
 
     // Viewport settings
     let viewport_height = 2.0;
-    let viewport_width = ASPECT_RATIO as f32 * viewport_height;
+    let viewport_width = 3.3; //ASPECT_RATIO as f32 * viewport_height;
     let focal_length = 1.0;
 
-    let origin: point3::Point3 = point3::Point3 {
+    let origin: Point3 = Point3 {
         x: 0.0,
         y: 0.0,
         z: 0.0,
     };
 
-    let horizontal: vector3::Vector3 = vector3::Vector3 {
+    let horizontal: Vector3 = Vector3 {
         x: viewport_width,
         y: 0.0,
         z: 0.0,
     };
 
-    let vertical: vector3::Vector3 = vector3::Vector3 {
+    let vertical: Vector3 = Vector3 {
         x: 0.0,
         y: viewport_height,
         z: 0.0,
@@ -68,9 +68,19 @@ fn get_pixels() -> Vec<Color>{
             let u: f32 = i as f32 / (WIDTH as f32 - 1.0);
             let v: f32 = j as f32 / (HEIGHT as f32 - 1.0);
             
-            let r: ray::Ray = ray::Ray { origin: origin, direction: lower_left_corner - origin + horizontal * u + vertical * v };
+            let r: Ray = Ray { origin: origin, direction: lower_left_corner - origin + horizontal * u + vertical * v };
 
-            let color = RayColor(r);
+            let mut color = ray_color(r);
+
+            let center: Point3 = Point3 {
+                x: 0.0,
+                y: 0.0,
+                z: -1.0,
+            };
+
+            if (hit_sphere(center, 0.5, r)){
+                color = Color::BLACK;
+            }
 
             pixels[i * HEIGHT + j] = color;
         }
@@ -78,8 +88,17 @@ fn get_pixels() -> Vec<Color>{
     return pixels;
 }
 
-fn RayColor(r: ray::Ray) -> Color {
+fn ray_color(r: Ray) -> Color {
     let unit_direction: Vector3 = r.direction;
     let t = 0.5*(unit_direction.y + 1.0);
     Color{r: 255, g: 255, b: 255} * (1.0-t) + Color{r: 127, g: 180, b: 255} * t
+}
+
+fn hit_sphere(center: Point3, radius: f32, r: Ray) -> bool {
+    let oc: Vector3 = r.origin - center;
+    let a: f32 = oc.dot(oc) - radius * radius;
+    let b: f32 = 2.0 * oc.dot(r.direction);
+    let c: f32 = r.direction.dot(r.direction);
+    let discriminant: f32 = b * b - 4.0 * c * a;
+    discriminant > 0.0
 }
--- src/vector3.rs
+++ src/vector3.rs
@@ -151,6 +151,10 @@ impl Vector3 {
         (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
     }
 
+    pub fn dot(&self, other: Vector3) -> f32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+
 }
 
 impl Display for Vector3 {
