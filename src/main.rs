312947201454935242ec50394e79a5492801a14c
Zéphyr <antoine.familleroger@gmail.com>
2022-07-06 20:59:33 +0200

    Save intermediate parallelism

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate sdl2; 
 
-use sdl2::pixels::Color;
+use sdl2::{pixels::Color, video::Window};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::rect::Point;
-use std::time::Duration;
+
+use std::time::{SystemTime, Duration};
+use rayon::prelude::*;
 
 mod vector3;
 use vector3::Vector3;
@@ -13,9 +15,9 @@ use point3::Point3;
 mod ray;
 use ray::Ray;
 
-const WIDTH: u32 = 800;
+const WIDTH: u32 = 1200;
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
-const HEIGHT: u32 = 450;
+const HEIGHT: u32 = 675;
  
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -32,11 +34,41 @@ pub fn main() {
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
+    let mut last_frame = SystemTime::now() - Duration::from_millis(1);
+
+    // Viewport settings
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO as f32 * viewport_height;
+    let focal_length = 1.0;
+
+    let origin: Point3 = Point3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    let horizontal: Vector3 = Vector3 {
+        x: viewport_width,
+        y: 0.0,
+        z: 0.0,
+    };
+
+    let vertical: Vector3 = Vector3 {
+        x: 0.0,
+        y: viewport_height,
+        z: 0.0,
+    };
+
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - vector3 :: Vector3{x: 0.0, y: 0.0, z: focal_length};
+
+
+
+
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        //compute fps
+        println!("fps: {}", 1000 / SystemTime::now().duration_since(last_frame).unwrap().as_millis());
+        last_frame = SystemTime::now();
+        
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -49,55 +81,34 @@ pub fn main() {
 
         ////////////////////////////////////////////////////////////////////////
 
-        // Viewport settings
-        let viewport_height = 2.0;
-        let viewport_width = ASPECT_RATIO as f32 * viewport_height;
-        let focal_length = 1.0;
+        let indexes = 0 .. HEIGHT * WIDTH;
 
-        let origin: Point3 = Point3 {
-            x: 0.0,
-            y: 0.0,
-            z: 0.0,
-        };
+        //array of (Point, Color) of size WIDTH * HEIGHT
+        let mut pixels: Vec<(Point, Color)> = vec![];
 
-        let horizontal: Vector3 = Vector3 {
-            x: viewport_width,
-            y: 0.0,
-            z: 0.0,
-        };
+        indexes.into_par_iter().for_each(|x| {
+            let i = x % WIDTH;
+            let j = x / WIDTH;
+            let u: f32 = i as f32 / (WIDTH as f32 - 1.0);
+            let v: f32 = j as f32 / (HEIGHT as f32 - 1.0);
+            
+            let r: Ray = Ray { origin: origin, direction: lower_left_corner - origin + horizontal * u + vertical * v };
 
-        let vertical: Vector3 = Vector3 {
-            x: 0.0,
-            y: viewport_height,
-            z: 0.0,
-        };
+            let mut color = ray_color(r);
+            
+            let p: Point = Point::new(i as i32, (HEIGHT - j - 1) as i32);
 
-        let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - vector3 :: Vector3{x: 0.0, y: 0.0, z: focal_length};
+            pixels.push((p, color));
+        });
 
-        for j in 0..HEIGHT {
-            for i in 0..WIDTH {
-    
-                let u: f32 = i as f32 / (WIDTH as f32 - 1.0);
-                let v: f32 = j as f32 / (HEIGHT as f32 - 1.0);
-                
-                let r: Ray = Ray { origin: origin, direction: lower_left_corner - origin + horizontal * u + vertical * v };
-    
-                let mut color = ray_color(r);
-                
-                let p: Point = Point::new(i as i32, (HEIGHT - j - 1) as i32);
-                canvas.set_draw_color(color);
-                canvas.draw_point(p);
-    
-            }
-        }
-
-
-        
+        pixels.iter().for_each(|p|{
+            canvas.set_draw_color(p.1);
+            canvas.draw_point(p.0);
+        });
 
         ////////////////////////////////////////////////////////////////////////
 
         canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
 
