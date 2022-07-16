fb0d5e9ced61154b581aa1459c1ab6c9f9b69777
Zéphyr <antoine.familleroger@gmail.com>
2022-07-16 23:44:40 +0200

    Added intermediate pixelisation

--- src/main.rs
+++ src/main.rs
@@ -36,11 +36,36 @@ mod object {
 use object::camera::Camera;
 
 
+static SP1: &'static Sphere = &Sphere {
+    center: Point3 {
+        x: 0.0,
+        y: 0.0,
+        z: -1.0,
+    },
+    radius: 0.5,
+};
+
+static SP2: &'static Sphere = &Sphere {
+    center: Point3 {
+        x: 0.0,
+        y: -100.5,
+        z: -1.0,
+    },
+    radius: 100.0,
+};
+
+static NUMBERS: &'static [&Sphere] = &[SP1, SP2];
+
+
+
 pub fn main() {
+    
+    let PIXEL_SIZE: u32 = 8;
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
-    let window = video_subsystem.window("Raytracer", 600, 300)
+    let window = video_subsystem.window("Raytracer", 640, 320)
         .position_centered()
         .resizable()
         .build()
@@ -76,7 +101,7 @@ pub fn main() {
 
         ////////////////////////////////////////////////////////////////////////
 
-        let mut pixels: Vec<(Point, Color)> = vec![(Point::new(0, 0), Color::BLACK); (height * width) as usize];
+        let mut pixels: Vec<(Point, Color)> = vec![(Point::new(0, 0), Color::BLACK); (height * width / (PIXEL_SIZE * PIXEL_SIZE)) as usize];
 
         let sphere1 = Box::new(Sphere {
             center: Point3 {
@@ -96,57 +121,68 @@ pub fn main() {
             radius: 100.0,
         });
 
-        let world: Vec<Box<dyn Hittable>> = vec![sphere1, sphere2];
-        println!("{}", pixels.len());
+        let world = vec![sphere1, sphere2];
+
+
         pixels.par_iter_mut().enumerate().for_each(|(k, p)| {
-            println!("{}", k);
-            let i = k as u32 % width;
-            let j = k as u32 / width;
+            let i = k as u32 % (width / PIXEL_SIZE);
+            let j = k as u32 / (width / PIXEL_SIZE);
+            
+            //println!("{}", NUMBERS[0]);
             
             let mut color = Color::BLACK; 
             let mut r: u16 = 0;
             let mut g: u16 = 0;
             let mut b: u16 = 0;
 
-            let steps = 1;
+            let steps = 50;
             
-            /*for _ in 0..steps {
-                let u: f32 = (i as f32 + random_double()) / (width as f32 - 1.0);
-                let v: f32 = (j as f32 + random_double()) / (height as f32 - 1.0);
+            for _ in 0..steps {
+                let u: f32 = (i as f32 + random_double()) / ((width / PIXEL_SIZE) as f32 - 1.0);
+                let v: f32 = (j as f32 + random_double()) / ((width / (PIXEL_SIZE * 2)) as f32 - 1.0);
 
                 let ray: Ray = camera.get_ray(u, v);
 
-                r += ray_color(&ray, &world, 5).r as u16;
-                g += ray_color(&ray, &world, 5).g as u16;
-                b += ray_color(&ray, &world, 5).b as u16;
+                r += ray_color(&ray, NUMBERS, 2).r as u16;
+                g += ray_color(&ray, NUMBERS, 2).g as u16;
+                b += ray_color(&ray, NUMBERS, 2).b as u16;
             }
-            */
-            let u: f32 = i as f32 / (width as f32 - 1.0);
-            let v: f32 = j as f32 / (height as f32 - 1.0);
-            let ray: Ray = camera.get_ray(u, v);
-            r = ray_color(&ray, &world, 5).r as u16;
-            g = ray_color(&ray, &world, 5).g as u16;
-            b = ray_color(&ray, &world, 5).b as u16;
 
-            /*let scale = 1.0 / steps as f32;
+            let scale = 1.0 / steps as f32;
 
-            r = (r as f32 * scale).sqrt() as u16;
-            g = (g as f32 * scale).sqrt() as u16;
-            b = (b as f32 * scale).sqrt() as u16;*/
-            //r.clamp(0, 254);
-            //g.clamp(0, 254);
-            //b.clamp(0, 254);
+            //r = (r as f32 * scale).sqrt() as u16;
+            //g = (g as f32 * scale).sqrt() as u16;
+            //b = (b as f32 * scale).sqrt() as u16;
+            r /= steps as u16;
+            g /= steps as u16;
+            b /= steps as u16;
+
+            r.clamp(0, 254);
+            g.clamp(0, 254);
+            b.clamp(0, 254);
+
             color = Color::RGB(r as u8, g as u8, b as u8);
             
-            let point: Point = Point::new(i as i32, (height - j - 1) as i32);
+            let point: Point = Point::new(i as i32, ((height / PIXEL_SIZE) - j - 1) as i32);
 
             *p = (point, color);
         });
 
         
-        pixels.iter().for_each(|p|{
+        pixels.iter().enumerate().for_each(|(k, p)|{
             canvas.set_draw_color(p.1);
-            canvas.draw_point(p.0);
+
+            let i: u32 = PIXEL_SIZE * (k as u32 % (width / PIXEL_SIZE)) ;
+            let j: u32 = (height) - (PIXEL_SIZE * k as u32 / (width / PIXEL_SIZE));
+
+            //for loop of size PIXEL_SIZE * PIXEL_SIZE
+            for x in 0..PIXEL_SIZE {
+                for y in 0..PIXEL_SIZE {
+                    canvas.draw_point(Point::new(i as i32 + x as i32, j as i32 + y as i32)).unwrap();
+                }
+            }
+
+            //canvas.draw_point(p.0);
         });
         
         
@@ -157,7 +193,7 @@ pub fn main() {
     }
 }
 
-fn ray_color(ray: &Ray, world: &Vec<Box<dyn Hittable>>, depth: u8) -> Color {
+fn ray_color(ray: &Ray, world: &'static [&Sphere], depth: u8) -> Color {
 
     if depth <= 0 {
         return Color::BLACK;
@@ -197,7 +233,7 @@ fn ray_color(ray: &Ray, world: &Vec<Box<dyn Hittable>>, depth: u8) -> Color {
     Color::RGB(r as u8, g as u8, b as u8)
 }
 
-fn hit_anything (objects: &Vec<Box<dyn Hittable>>, r: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool {
+fn hit_anything (objects: &'static [&Sphere], r: &Ray, t_min: f32, t_max: f32, hit: &mut Hit) -> bool {
     let mut temp_hit: Hit = Hit{
         t: 0.0,
         point: Point3{x: 0.0, y: 0.0, z: 0.0},
